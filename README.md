79dec9146e66feec3c2a20b2e6d3481005a7453b
joshdail <joshdail@hotmail.com>
2022-01-21 22:59:12 -0500

    Change number of colors displayed from 5 to 15

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Tkinter utility to generate color palletes from jpg and png files
 
 ## Required packages: Tkinter, Pillow, Numpy
 
-This app allows you to generate a color palette based on the five most commonly occurring hex codes in a selected jpg or png file. (PDF files are not supported.) When you select an image, thumbnails of the five most commonly detected colors in the image along with their corresponding hex codes are displayed in the app window.
+This app allows you to generate a color palette based on the five most commonly occurring hex codes in a selected jpg or png file. (PDF files are not supported.) When you select an image, thumbnails of the 15 most commonly detected colors in the image along with their corresponding hex codes are displayed in the app window.
 
 ## Future:
 - Ability to specify the number of colors displayed
--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from windows import set_dpi_awareness
 
 # Note: Due to DPI awareness, the font size needs to be reasonably large
 THEME_FONT = ("Arial", 22, "normal")
+NUM_VALUES = 15
 
 
 class ImageReader:
@@ -22,12 +23,12 @@ class ImageReader:
         self.canvas = Canvas(width=1000, height=750, bg="#000")
         self.canvas.grid(row=0, column=0)
 
-        self.button = Button(text="Select Image", font=THEME_FONT, command=self.get_hex_codes)
+        self.button = Button(text="Select Image", font=THEME_FONT, command=self.get_color_values)
         self.button.grid(row=1, column=0)
 
         self.window.mainloop()
 
-    def get_hex_codes(self):
+    def get_color_values(self):
         img = PIL.Image.open(fp=filedialog.askopenfilename())
         img_array = array(img)
         rgb_values = []
@@ -36,18 +37,23 @@ class ImageReader:
             rgb_values = [value.tolist() for value in row]
 
         hex_values = [rgb2hex(value[0], value[1], value[2]) for value in rgb_values]
-        top_values = Counter(hex_values).most_common(5)
-        self.display_hex_codes(top_values)
+        top_values = Counter(hex_values).most_common(NUM_VALUES)
+        self.display_color_values(top_values)
 
-    def display_hex_codes(self, values):
-        starting_y = 0
+    def display_color_values(self, values):
         # Clear out any existing colors or hex codes
         self.canvas.delete("color")
         self.canvas.delete("hex_code")
+        starting_x = 0
+        starting_y = 0
         for value in values:
             color = value[0]
-            self.canvas.create_rectangle(0, starting_y, 150, starting_y + 150, fill=color, tags="color")
-            self.canvas.create_text(250, starting_y + 75, fill='#fff', font=THEME_FONT, text=color, tags="hex_code")
+            self.saved_colors.append(color)
+            self.canvas.create_rectangle(starting_x, starting_y, starting_x + 150, starting_y + 150, fill=color, tags="color")
+            self.canvas.create_text(starting_x + 235, starting_y + 75, fill='#fff', font=THEME_FONT, text=color, tags="hex_code")
             starting_y += 150
+            if starting_y >= 750:
+                starting_x += 333
+                starting_y = 0
 
 ImageReader()
